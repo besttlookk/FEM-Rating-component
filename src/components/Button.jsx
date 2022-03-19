00d1f8fd52544d5ca54b2890ced41dc1fdb7befb
Prabhash <besttlookk@gmail.com>
2022-03-19 11:08:58 +0530

    button disabled when no rating added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,17 @@ import React, { useContext } from "react";
 import { AppContext } from "../contexts/appContext";
 
 const Button = () => {
-  const { setStep } = useContext(AppContext);
+  const { setStep, rating } = useContext(AppContext);
+
+  const handleClick = () => {
+    if (rating === 0) return;
+    setStep(2);
+  };
   return (
     <button
-      className="block w-full pt-3 pb-2 font-bold text-white uppercase transition duration-500 transform rounded-full shadow-lg hover:text-primary hover:bg-white active:translate-y-1 text-primary-size bg-primary"
-      onClick={() => setStep(2)}
+      className="block w-full pt-3 pb-2 font-bold text-white uppercase transition duration-500 transform rounded-full shadow-lg hover:text-primary disabled:cursor-not-allowed disabled:bg-orange-300 disabled:text-white hover:bg-white active:translate-y-1 text-primary-size bg-primary"
+      onClick={() => handleClick}
+      disabled={rating === 0 ? true : false}
     >
       Submit
     </button>
